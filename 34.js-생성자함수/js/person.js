e93b77c439ec36e7cb5227c889eecfabd83f34c1
Mirinem <pjm8584@naver.com>
2023-05-18 18:19:57 +0900

    update

---
+++ 34.js-생성자함수/js/person.js
@@ -0,0 +1,12 @@
+// 해당하는 .js파일을 설치(연결)한다.
+
+function Person(userName,userAge){
+    this.name = userName
+    this.age = userAge
+    this.sayHello = function(){
+        document.write("<p>안녕하세요 "+this.name+"입니다.</p>")
+    }
+    this.intro =function(){
+        document.write(`<p>${this.name}님은 ${this.age}살입니다.</p>`)
+    }
+}
