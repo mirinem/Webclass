d9012d60f8523334761cdcd15d9345d1f2409a61
Mirinem <pjm8584@naver.com>
2023-06-12 21:22:32 +0900

    new update

--- 39.js-effect/js/study4.js
+++ 39.js-effect/js/study4.js
@@ -1,21 +1,26 @@
 
 $(document).ready(function(){
-
+    
+    // 바로 아래 연산이 실행되려면 다음아래 연산이 필요하다
     $(".list>li").each(function(){
-        let result = Math.round(Math.random())
-        if(result){
+        let result = Math.round(Math.random()*1000)%4
+        if(result==0){
             $(this).addClass("mltr")
+        }else if(result==1){
+            $(this).addClass("mrtl")
+        }else if(result==2){
+            $(this).addClass("mbtt")
         }else{
-            $(this).addClass("downup")
+            $(this).addClass("mttb")
         }
         
     })
-
+    
     $(window).scroll(function(){
         let winst = $(window).scrollTop() //스크롤바가 위에서 얼만큼 내려왔는지 계산
         let winHeight = $(window).height()*0.65 // 브라우저화면의 높이를 계산
 
-        $(".mltr,.mrtl,.downup").each(function(){
+        $(".mltr,.mrtl,.mbtt,.mttb").each(function(){
             if(winst+winHeight>$(this).offset().top){
                 $(this).addClass("on")
             }else{
--- 39.js-effect/js/study5.js
+++ 39.js-effect/js/study5.js
@@ -1,6 +1,72 @@
 $(document).ready(function(){
 
     let count = 0;
+    
+    let perView;
+    // train클래스 너비를 변경 => train클래스의 너비를 불러와서 perView로 나눈 후 결과를 다시 적용
+    let stationWidth;
+    let trainWidth;
+    
+    // 최초의 문서가 완성 됐을 떄, 한번 실행되도록
+    let winWidth = $(window).width()
+
+        if(winWidth>=1024){
+            console.log("pc사이즈입니다")
+            perView = 2.5
+            stationWidth = $(".station").width()
+            trainWidth = stationWidth * 5 / perView
+            $(".train").width(trainWidth)
+           
+        }else if(winWidth>=768){
+            console.log("태블릿사이즈입니다")
+            perView = 2
+            stationWidth = $(".station").width()
+            trainWidth = stationWidth * 5 / perView
+            $(".train").width(trainWidth)
+
+        }else{
+            console.log("모바일사이즈입니다")
+            perView = 1
+            stationWidth = $(".station").width()
+            trainWidth = stationWidth * 5 / perView
+            $(".train").width(trainWidth)
+
+        }
+
+   
+
+    // 화면사이즈가 줄어들 때 실행
+    $(window).resize(function(){
+
+        let winWidth = $(window).width()
+        // console.log(winWidth)
+
+        if(winWidth>=1024){
+            console.log("pc사이즈입니다")
+            perView = 2.5
+            stationWidth = $(".station").width()
+            trainWidth = stationWidth * 5 / perView
+            $(".train").width(trainWidth)
+           
+        }else if(winWidth>=768){
+            console.log("태블릿사이즈입니다")
+            perView = 2
+            stationWidth = $(".station").width()
+            trainWidth = stationWidth * 5 / perView
+            $(".train").width(trainWidth)
+
+        }else{
+            console.log("모바일사이즈입니다")
+            perView = 1
+            stationWidth = $(".station").width()
+            trainWidth = stationWidth * 5 / perView
+            $(".train").width(trainWidth)
+
+        }
+
+    })
+
+
     $(".btnNext").click(function(e){
         count++
         e.preventDefault()
